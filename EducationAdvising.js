9b66fdf53b4edaa8996c3d398993eb7ed97e6435
SarjuPoudel <106352241+SarjuPoudel@users.noreply.github.com>
2022-05-30 21:42:06 -0500

    Add files via upload

---
+++ EducationAdvising.js
@@ -0,0 +1,8 @@
+function NewStudent(){
+    let Welcomeparagraph=document.createElement("div");
+    document.body.appendChild(Welcomeparagraph);
+    let textnode=document.createTextNode("Welcome to the Education dungeon")
+    Welcomeparagraph.appendChild(textnode);
+    
+    
+}
